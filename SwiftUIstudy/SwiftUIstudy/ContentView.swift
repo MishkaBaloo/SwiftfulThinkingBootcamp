d351b887f7943e65116c24a3deaca2dc474698d7
Michael Tokmakov <94358664+MishkaBaloo@users.noreply.github.com>
2024-03-28 21:22:39 +0200

    Add files via upload

---
+++ SwiftUIstudy/SwiftUIstudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIstudy
+//
+//  Created by user on 28.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIstudy/SwiftUIstudy/SwiftUIstudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIstudyApp.swift
+//  SwiftUIstudy
+//
+//  Created by user on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIstudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
