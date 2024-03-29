8e2357a0a50773b64b920fe95ee11d61a7dbcbfd
Mykhailo Tokmakov <mishkabalooo@gmail.com>
2024-03-29 20:58:10 +0200

    Shapes bootcamp

---
+++ SwiftUIstudy/SwiftUIstudy/Shapes bootcamp.swift
@@ -0,0 +1,30 @@
+//
+//  Shapes bootcamp.swift
+//  SwiftUIstudy
+//
+//  Created by user on 29.03.2024.
+//
+
+import SwiftUI
+
+struct Shapes_bootcamp: View {
+    var body: some View {
+       // Circle()
+        //Ellipse()
+        //Capsule(style: .circular)
+        RoundedRectangle(cornerRadius: 10)
+            //.fill(Color.green)
+            //.foregroundColor(.pink)
+            //.stroke()
+            //.stroke(Color.red)
+            //.stroke(Color.blue, lineWidth: 30)
+            //.stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: .round, dash: [30]))
+            .trim(from: 0.4, to: 1.0)
+            //.stroke(Color.purple, lineWidth: 50)
+            .frame(width: 300, height: 200)
+    }
+}
+
+#Preview {
+    Shapes_bootcamp()
+}
