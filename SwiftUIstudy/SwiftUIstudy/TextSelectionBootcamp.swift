9387b67b0385f1904e848f683161bae6b8cfbccf
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-02 14:39:06 +0300

    TextSelection Bootcamp

---
+++ SwiftUIstudy/SwiftUIstudy/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUIstudy
+//
+//  Created by Michael on 02.05.2024.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
