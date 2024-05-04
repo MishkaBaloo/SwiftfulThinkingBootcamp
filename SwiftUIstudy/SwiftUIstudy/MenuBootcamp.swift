e1b152cfe4756248913dd47f8d5357b17cd6c42f
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-05 00:42:50 +0300

    Menu Bootcamp

---
+++ SwiftUIstudy/SwiftUIstudy/MenuBootcamp.swift
@@ -0,0 +1,31 @@
+//
+//  MenuBootcamp.swift
+//  SwiftUIstudy
+//
+//  Created by Michael on 05.05.2024.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click me!") {
+            Button("One") {
+                
+            }
+            Button("Two") {
+                
+            }
+            Button("Three") {
+                
+            }
+            Button("Four") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
